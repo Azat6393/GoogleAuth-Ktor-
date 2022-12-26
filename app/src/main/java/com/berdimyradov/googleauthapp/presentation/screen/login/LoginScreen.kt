28751cac550d40d40b76b0b2eec3733f33fcfba3
Azat Berdimyradov <iazat6393@gmail.com>
2022-12-26 18:37:27 +0300

    Implemented One-Tap Google sign in

--- app/src/main/java/com/berdimyradov/googleauthapp/presentation/screen/login/LoginScreen.kt
+++ app/src/main/java/com/berdimyradov/googleauthapp/presentation/screen/login/LoginScreen.kt
@@ -1,10 +1,14 @@
 package com.berdimyradov.googleauthapp.presentation.screen.login
 
+import android.app.Activity
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.berdimyradov.googleauthapp.presentation.screen.common.StartActivityForResult
+import com.berdimyradov.googleauthapp.presentation.screen.common.signIn
 
 @Composable
 fun LoginScreen(
@@ -26,4 +30,29 @@ fun LoginScreen(
             )
         }
     )
+
+    val activity = LocalContext.current as Activity
+
+    StartActivityForResult(
+        key = signInState,
+        onResultReceived = { tokenId ->
+
+        },
+        onDialogDismissed = {
+            loginViewModel.saveSignedInState(signedIn = false)
+        },
+    ) { activityLauncher ->
+        if (signInState) {
+            signIn(
+                activity = activity,
+                launchActivityResult = { intentSenderRequest ->
+                    activityLauncher.launch(intentSenderRequest)
+                },
+                accountNotFound = {
+                    loginViewModel.saveSignedInState(signedIn = false)
+                    loginViewModel.updateMessageBarState()
+                }
+            )
+        }
+    }
 }
--- app/src/main/java/com/berdimyradov/googleauthapp/util/Constants.kt
+++ app/src/main/java/com/berdimyradov/googleauthapp/util/Constants.kt
@@ -5,4 +5,6 @@ object Constants {
 
     const val PREFERENCES_SIGNED_IN_KEY = "signed_in_key"
     const val PREFERENCES_NAME = "app_preferences"
+
+    const val CLIENT_ID = "261708211161-taijichmjo6ajgn44d9tr5imqb9c1sn0.apps.googleusercontent.com"
 }
